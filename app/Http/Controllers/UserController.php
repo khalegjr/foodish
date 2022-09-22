82833c378364278c41c0b5ff1500c4a998c40d7c
Khaled <khalegjr@gmail.com>
2022-09-22 16:47:42 -0300

    feat(user): Validando campos do form de cadastro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function store(Request $request) {
-        return "${request}";
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'min:2', 'max:255'],
+            'email' => ['required', 'email:rfc,dns', 'string', 'unique:users', 'max:255'],
+            'phone' => ['string', 'max:14', 'nullable'],
+            'city' => ['string', 'max:255', 'nullable'],
+            'uf' => ['string', 'max:2', 'nullable']
+        ]);
+
+        return response()->json([
+            'message' => $validated
+        ]);
     }
 }
