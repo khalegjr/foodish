9c1cc7d0750f077dac03987f10b5c796cc7e8b61
Khaled <khalegjr@gmail.com>
2022-09-22 17:43:02 -0300

    refactor(user): Usa o form de validação e cadastra o usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,22 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function store(Request $request) {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'min:2', 'max:255'],
-            'email' => ['required', 'email:rfc,dns', 'string', 'unique:users', 'max:255'],
-            'phone' => ['string', 'max:14', 'nullable'],
-            'city' => ['string', 'max:255', 'nullable'],
-            'state' => ['string', 'max:2', 'nullable']
-        ]);
+    public function store(StoreUserRequest $request) {
 
-        return response()->json([
-            'message' => $validated
-        ]);
+        return User::create($request->all());
     }
 }
