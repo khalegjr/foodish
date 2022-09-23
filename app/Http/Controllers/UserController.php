71ecb3ab4306e255f6fb80a0095e10ecfa5a4e59
Khaled <khalegjr@gmail.com>
2022-09-23 03:20:56 -0300

    refactor(user): Melhora o retorno de erros da API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,20 @@ use App\Models\User;
 class UserController extends Controller
 {
     public function store(StoreUserRequest $request) {
+        try {
+            $user = User::create($request->all());
 
-        return User::create($request->all());
+            return response()->json([
+                'data' => [
+                    'msg' => 'Usuário cadastrado com sucesso!'
+                ]
+            ], 201);
+        } catch (\Exception $e) {
+            if(config('app.debug')) {
+                return response()->json(['data' => ['error' => $e->getMessage(), 'code' => 1010]], 500);
+            }
+
+            return response()->json(['data' => ['error' => "Houve um erro ao realizar a operação de criação", 'code' => 1010]], 500);
+        }
     }
 }
--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreUserRequest extends FormRequest
 {
@@ -30,5 +32,25 @@ class StoreUserRequest extends FormRequest
             'city' => ['string', 'max:255', 'nullable'],
             'state' => ['string', 'max:2', 'nullable']
         ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'nome',
+            'email' => 'email',
+            'phone' => 'telefone',
+            'city' => 'cidade',
+            'state' => 'estado'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors()
+        ]));
     }
 }
