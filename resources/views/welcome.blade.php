71b5085f636bf1436987e6426c5028a1c4b489c2
Khaled <khalegjr@gmail.com>
2022-09-23 16:41:54 -0300

    fix: Apaga formulário de teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,32 +20,7 @@
 <body class="antialiased">
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <form action="{{ route('users.store') }}" method="post" enctype="multipart/form-data">
-            @csrf
-
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" required minlength="4" maxlength="100" size="10">
-
-            <label for="email">email</label>
-            <input type="text" id="email" name="email" required minlength="4" maxlength="100" size="10">
-
-            <input type="tel" id="phone" name="phone" maxlength="14" size="10">
-
-            <input type="text" id="city" name="city" maxlength="100" size="20">
-
-            <input type="text" id="state" name="state" maxlength="2" size="10">
-
-            <input type="submit" value="Enviar">
-        </form>
+        API FOODISH
     </div>
 </body>
 
